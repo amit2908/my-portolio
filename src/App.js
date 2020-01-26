56265826e67a768c2aa0ab215c0f94aa16352fe9
Shubham Ojha <ojhashubham29@gmail.com>
2020-01-26 18:14:06 +0530

    Navigation bar UI Design

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './screens/Home'
+import Home from './screens/Home';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        This is Shubham Ojha
-      </header>
+      <Home />
     </div>
   );
 }
