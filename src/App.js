3a1dc2f3f2be6a0f31074d5f61b335fab8a60a05
Shubham Ojha <ojhashubham29@gmail.com>
2020-01-28 07:48:18 +0530

    Refinement

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import {
   BrowserRouter as Router,
   Switch,
-  Route,
-  Link
+  Route
 } from "react-router-dom";
 
 import Root from './screens/Root';
--- src/screens/ContactMe.js
+++ src/screens/ContactMe.js
@@ -10,7 +10,7 @@ function ContactMe() {
             <div id="boxshadow" className="Card"><Email id="icon"/>ojhashubham29@gmail.com <br/> ojhashubham29@yahoo.com </div>
             <div id="boxshadow" className="Card"><SmartphoneIcon id="icon"/> +918983061702 <br/> +917389844740</div>
             <div id="boxshadow" className="Card"><LinkedInIcon id="icon"/> <a href="https://linkedin.com/in/shubhamojha2908/">shubhamojha2908</a></div>
-            <div id="boxshadow" className="Card"><img id="icon" src="https://img.icons8.com/material-rounded/24/000000/skype.png" />ojhashubham29</div>
+            <div id="boxshadow" className="Card"><img id="icon" alt="Skype" src="https://img.icons8.com/material-rounded/24/000000/skype.png" />ojhashubham29</div>
             <div id="boxshadow" className="Card"><Facebook id="icon"/><a href="https://www.facebook.com/shubham.ojha.39">shubham.ojha.39</a></div>
         </div>
     );
